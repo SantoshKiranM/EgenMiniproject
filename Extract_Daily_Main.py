85f0ad4547c61f82ee0041fb49f2d5c1d9ed83a6
SantoshKiranM <67682401+SantoshKiranM@users.noreply.github.com>
2021-10-17 22:17:36 -0500

    Update Extract_Daily_Main.py

--- Extract_Daily_Main.py
+++ Extract_Daily_Main.py
@@ -1,7 +1,21 @@
+import multiprocessing
+from multiprocessing import Process
 import Extract_Daily_CaseCount_MultiThreading
 extract = Extract_Daily_CaseCount_MultiThreading.CaseCountExtract()
 
-extract.create_request()
-extract.create_tables()
-extract.load_tables()
+responseList = extract.create_request()
+extract.create_tables(responseList)
+
+#without multiprocessing
+#for response in responseList:
+#    extract.sample_fun(response)
+
+#multiprocessing
+PROCESSES = multiprocessing.cpu_count()
+with multiprocessing.Pool(PROCESSES) as p:
+    result = p.map_async(extract.load_table,responseList)
+    print(result.get())
+    p.close()
+    p.join()
+
 extract.validate_close()
